c13c1575f37bd26523e86c6eb66c9b666bd1fa96
Xarien <Xarien@169.254.146.14>
2017-04-27 15:25:41 +0200

    Add 1!

--- Gti-test-project/src/by/xar/test/Main.java
+++ Gti-test-project/src/by/xar/test/Main.java
@@ -3,7 +3,7 @@ package by.xar.test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from Git Hun!!!");
+		System.out.println("1Hello from Git Hun!!!");
 
 	}
 
